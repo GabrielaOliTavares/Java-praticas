1fdade29e36dfc8a7ff88bc06b13118170906286
GabrielaOliTavares <145030166+GabrielaOliTavares@users.noreply.github.com>
2024-04-08 23:50:33 +0000

    $git commit

---
+++ pratica1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class fatorial {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite até que numero deseja receber o fatorial: ");
+        int numeroFinal = entrada.nextInt();
+
+        for(i = 0; i <= numeroFinal; i++){
+            System.out.println("Fatorial de: " + i)
+            for(j = 0; j <= 10; j++) {
+                System.out.format("")
+            }
+
+        }
+    }
+
+
+
+}
